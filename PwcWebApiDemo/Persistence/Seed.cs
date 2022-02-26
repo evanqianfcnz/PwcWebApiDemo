17aa21482f6af118d793a9bf7ca30c1111609c6b
evanqianfcnz <98140807+evanqianfcnz@users.noreply.github.com>
2022-02-27 12:23:56 +1300

    Db Migrate

---
+++ PwcWebApiDemo/Persistence/Seed.cs
@@ -0,0 +1,50 @@
+﻿using PwcWebApiDemo.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PwcWebApiDemo.Persistence
+{
+    public class Seed
+    {
+        public static async Task SeedData(DataContext context)
+        {
+            if (context.Clients.Any()) return;
+            var clients = new List<Client>
+            {
+                new Client
+                {
+                    Id=Guid.NewGuid(),
+                    Name = "IBM",
+                    Email="contact@ibm.com",
+                    StartFrom= new DateTime(1990,1,2),
+                },
+                new Client
+                {
+                    Id=Guid.NewGuid(),
+                    Name = "HP",
+                    Email="contact@hp.com",
+                    StartFrom= new DateTime(1991,2,3),
+                },
+                new Client
+                {
+                    Id=Guid.NewGuid(),
+                    Name = "SAP",
+                    Email="contact@sap.com",
+                    StartFrom= new DateTime(1992,3,4),
+                },
+                new Client
+                {
+                    Id=Guid.NewGuid(),
+                    Name = "Oracle",
+                    Email="contact@oracle.com",
+                    StartFrom= new DateTime(1993,4,5),
+                }
+            };
+
+            await context.Clients.AddRangeAsync(clients);
+            await context.SaveChangesAsync();
+        }
+    }
+}
